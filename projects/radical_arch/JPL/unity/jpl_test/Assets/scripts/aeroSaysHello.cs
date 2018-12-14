67f7fd1b28a5363decfe48a6ba0c257a0969566a
datarulesme <thecynthiaandrews@gmail.com>
2018-12-14 15:47:36 -0500

    updated bio

---
+++ projects/radical_arch/JPL/unity/jpl_test/Assets/scripts/aeroSaysHello.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aeroSaysHello : MonoBehaviour {
+
+	public AudioSource helloFromAero;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			helloFromAero.Play ();
+		}
+	}
+}
